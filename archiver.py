aa3bf9fd4c41c78507a5f2b749ec42aed64af590
Dmitry Duev <dmitry.duev@gmail.com>
2017-06-30 16:23:41 -0700

    work in progress

--- archiver.py
+++ archiver.py
@@ -1,4 +1,7 @@
+import argparse
 import signal
+
+import pytz
 from distributed import Client, LocalCluster
 from queue import Queue
 import time
@@ -10,6 +13,7 @@ import sys
 import json
 import logging
 import collections
+import pymongo
 
 
 class TimeoutError(Exception):
@@ -141,7 +145,7 @@ class Archiver(object):
             self.config = self.get_config(_config_file=config_file)
 
             ''' set up logging at init '''
-            self.logger, self.logger_utc_date = self.set_up_logging(_name='archive', _level=logging.INFO, _mode='a')
+            self.logger, self.logger_utc_date = self.set_up_logging(_name='archive', _mode='a')
 
             ''' initialize dask.distributed LocalCluster for distributed task processing '''
             # alternative, or if workers are to be run on different machines
@@ -167,6 +171,12 @@ class Archiver(object):
             self.futures = self.c.map(task_runner, self.q, maxsize=self.config['parallel']['n_workers'])
             self.results = self.c.gather(self.futures)  # Gather results
 
+            ''' DB connection is handled in subclass '''
+            self.db = None
+
+            ''' raw data is handled in subclass '''
+            self.raw_data = None
+
         except Exception as e:
             print(e)
             traceback.print_exc()
@@ -177,7 +187,6 @@ class Archiver(object):
         """
             Load config JSON file
         """
-        # raise NotImplementedError
         ''' script absolute location '''
         abs_path = os.path.dirname(inspect.getfile(inspect.currentframe()))
 
@@ -200,7 +209,7 @@ class Archiver(object):
             else:
                 raise Exception('Failed to load config file')
 
-    def set_up_logging(self, _name='archive', _level=logging.DEBUG, _mode='w'):
+    def set_up_logging(self, _name='archive', _mode='w'):
         """ Set up logging
 
             :param _name:
@@ -208,6 +217,20 @@ class Archiver(object):
             :param _mode: overwrite log-file or append: w or a
             :return: logger instance
             """
+        # 'debug', 'info', 'warning', 'error', or 'critical'
+        if self.config['misc']['logging_level'] == 'debug':
+            _level = logging.DEBUG
+        elif self.config['misc']['logging_level'] == 'info':
+            _level = logging.INFO
+        elif self.config['misc']['logging_level'] == 'warning':
+            _level = logging.WARNING
+        elif self.config['misc']['logging_level'] == 'error':
+            _level = logging.ERROR
+        elif self.config['misc']['logging_level'] == 'critical':
+            _level = logging.CRITICAL
+        else:
+            raise ValueError('Config file error: logging level must be ' +
+                             '\'debug\', \'info\', \'warning\', \'error\', or \'critical\'')
 
         # get path to logs from config:
         _path = self.config['path']['path_logs']
@@ -251,13 +274,301 @@ class Archiver(object):
         if datetime.datetime.utcnow().strftime('%Y%m%d') != self.logger_utc_date:
             # reset
             self.shut_down_logger()
-            self.logger, self.logger_utc_date = self.set_up_logging(_name='archive', _level=logging.INFO, _mode='a')
+            self.logger, self.logger_utc_date = self.set_up_logging(_name='archive', _mode='a')
+
+    def connect_to_db(self):
+        """
+            Connect to database. Specific details will differ for different users,
+            so this should be implemented in subclass
+        :return:
+        """
+        raise NotImplementedError
+
+    def disconnect_from_db(self):
+        """
+            Disconnect from database. Specific details will differ for different users,
+            so this should be implemented in subclass
+        :return:
+        """
+        raise NotImplementedError
+
+    def check_db_connection(self):
+        """
+            Check if DB connection is alive
+        :return:
+        """
+        raise NotImplementedError
+
+    def get_raw_data(self):
+        """
+            Parse sources containing raw data.
+        :return:
+        """
+        raise NotImplementedError
+
+    def cycle(self):
+        """
+            Main processing cycle to loop over dates and observations
+        :return:
+        """
+        raise NotImplementedError
+
+    def naptime(self):
+        """
+            Return time to sleep in seconds for the archiving engine
+            before waking up to rerun itself.
+             In the daytime, it's 1 hour
+             In the nap time, it's nap_time_start_utc - utc_now()
+        :return: time interval in seconds to sleep for
+        """
+        _config = self.config['misc']
+        try:
+            # local or UTC?
+            tz = pytz.utc if _config['nap_time_frame'] == 'UTC' else None
+            now = datetime.datetime.now(tz)
+
+            if _config['nap_at_night']:
+
+                last_midnight = datetime.datetime(now.year, now.month, now.day, tzinfo=tz)
+                next_midnight = datetime.datetime(now.year, now.month, now.day, tzinfo=tz) \
+                                + datetime.timedelta(days=1)
+
+                hm_start = list(map(int, _config['nap_time_start'].split(':')))
+                hm_stop = list(map(int, _config['nap_time_stop'].split(':')))
+
+                if hm_stop[0] < hm_start[0]:
+                    h_before_midnight = 24 - (hm_start[0] + hm_start[1] / 60.0)
+                    h_after_midnight = hm_stop[0] + hm_stop[1] / 60.0
+
+                    # print((next_midnight - now).total_seconds() / 3600.0, h_before_midnight)
+                    # print((now - last_midnight).total_seconds() / 3600.0, h_after_midnight)
+
+                    if (next_midnight - now).total_seconds() / 3600.0 < h_before_midnight:
+                        sleep_until = next_midnight + datetime.timedelta(hours=h_after_midnight)
+                        print('sleep until:', sleep_until)
+                    elif (now - last_midnight).total_seconds() / 3600.0 < h_after_midnight:
+                        sleep_until = last_midnight + datetime.timedelta(hours=h_after_midnight)
+                        print('sleep until:', sleep_until)
+                    else:
+                        sleep_until = now + datetime.timedelta(minutes=_config['loop_interval'])
+                        print('sleep until:', sleep_until)
+
+                else:
+                    h_after_midnight_start = hm_start[0] + hm_start[1] / 60.0
+                    h_after_midnight_stop = hm_stop[0] + hm_stop[1] / 60.0
+
+                    if (last_midnight + datetime.timedelta(hours=h_after_midnight_start) <
+                            now < last_midnight + datetime.timedelta(hours=h_after_midnight_stop)):
+                        sleep_until = last_midnight + datetime.timedelta(hours=h_after_midnight_stop)
+                        print('sleep until:', sleep_until)
+                    else:
+                        sleep_until = now + datetime.timedelta(minutes=_config['loop_interval'])
+                    print('sleep until:', sleep_until)
+
+                return (sleep_until - now).total_seconds()
+
+            else:
+                # sleep for loop_interval minutes otherwise (return seconds)
+                return _config['loop_interval'] * 60.0
+
+        except Exception as _e:
+            traceback.print_exc()
+            self.logger.error('Failed to take a nap, taking a pill to fall asleep for an hour.')
+            self.logger.error(_e)
+            return 3600.0
+
+    def sleep(self):
+        """
+            Take a nap in between cycles
+        :return:
+        """
+        sleep_for = self.naptime()  # seconds
+        # sleeping for longer than 10 minutes?
+        if sleep_for > 10*60:
+            self.logger.debug('Will disconnect from DB not to keep connection idle.')
+            self.disconnect_from_db()
+        self.logger.debug('Falling asleep for {:.1f} minutes.'.format(sleep_for / 60.0))
+        time.sleep(sleep_for)
 
 
 class RoboaoArchiver(Archiver):
+    """
+        A class representation of major data house-keeping/archiving tasks for
+        Robo-AO, a robotic laser guide star adaptive optics system
+    """
     def __init__(self, config_file=None):
-        # initialize super class
+        """
+            Init
+        :param config_file:
+        """
+        ''' initialize super class '''
         super(RoboaoArchiver, self).__init__(config_file=config_file)
+
+        ''' connect to db: '''
+        # will exit if this fails
+        self.connect_to_db()
+
+    def connect_to_db(self):
+        """
+            Connect to Robo-AO's MongoDB-powered database
+        :return:
+        """
+        _config = self.config
+        try:
+            if self.logger is not None:
+                self.logger.debug('Connecting to the Robo-AO database at {:s}:{:d}'.
+                                  format(_config['database']['host'], _config['database']['port']))
+            if _config['server']['environment'] == 'production':
+                _client = pymongo.MongoClient(host=_config['database']['host'], port=_config['database']['port'],
+                                              replicaset=_config['database']['replicaset'],
+                                              readPreference='primaryPreferred')
+            else:
+                # standalone from my laptop:
+                _client = pymongo.MongoClient(host=_config['database']['host'], port=_config['database']['port'])
+            # grab main database:
+            _db = _client[_config['database']['db']]
+        except Exception as _e:
+            if self.logger is not None:
+                self.logger.error(_e)
+                self.logger.error('Failed to connect to the Robo-AO database at {:s}:{:d}'.
+                                  format(_config['database']['host'], _config['database']['port']))
+            # raise error
+            raise ConnectionRefusedError
+        try:
+            _db.authenticate(_config['database']['user'], _config['database']['pwd'])
+            if self.logger is not None:
+                self.logger.debug('Successfully authenticated with the Robo-AO database at {:s}:{:d}'.
+                                  format(_config['database']['host'], _config['database']['port']))
+        except Exception as _e:
+            if self.logger is not None:
+                self.logger.error(_e)
+                self.logger.error('Authentication failed for the Robo-AO database at {:s}:{:d}'.
+                                  format(_config['database']['host'], _config['database']['port']))
+            raise ConnectionRefusedError
+        try:
+            _coll_obs = _db[_config['database']['collection_obs']]
+            if self.logger is not None:
+                self.logger.debug('Using collection {:s} with obs data in the database'.
+                                  format(_config['database']['collection_obs']))
+        except Exception as _e:
+            if self.logger is not None:
+                self.logger.error(_e)
+                self.logger.error('Failed to use a collection {:s} with obs data in the database'.
+                                  format(_config['database']['collection_obs']))
+            raise NameError
+        try:
+            _coll_aux = _db[_config['database']['collection_aux']]
+            if self.logger is not None:
+                self.logger.debug('Using collection {:s} with aux data in the database'.
+                                  format(_config['database']['collection_aux']))
+        except Exception as _e:
+            if self.logger is not None:
+                self.logger.error(_e)
+                self.logger.error('Failed to use a collection {:s} with aux data in the database'.
+                                  format(_config['database']['collection_aux']))
+            raise NameError
+        try:
+            _coll_usr = _db[_config['database']['collection_pwd']]
+            if self.logger is not None:
+                self.logger.debug('Using collection {:s} with user access credentials in the database'.
+                                  format(_config['database']['collection_pwd']))
+        except Exception as _e:
+            if self.logger is not None:
+                self.logger.error(_e)
+                self.logger.error('Failed to use a collection {:s} with user access credentials in the database'.
+                                  format(_config['database']['collection_pwd']))
+            raise NameError
+        try:
+            # build dictionary program num -> pi name
+            cursor = _coll_usr.find()
+            _program_pi = {}
+            for doc in cursor:
+                # handle admin separately
+                if doc['_id'] == 'admin':
+                    continue
+                _progs = doc['programs']
+                for v in _progs:
+                    # multiple users could have access to the same program, that's totally fine!
+                    if str(v) not in _program_pi:
+                        _program_pi[str(v)] = [doc['_id'].encode('ascii', 'ignore')]
+                    else:
+                        _program_pi[str(v)].append(doc['_id'].encode('ascii', 'ignore'))
+                        # print(program_pi)
+        except Exception as _e:
+            _program_pi = {}
+            if self.logger is not None:
+                self.logger.error(_e)
+
+        if self.logger is not None:
+            self.logger.debug('Successfully connected to the Robo-AO database at {:s}:{:d}'.
+                              format(_config['database']['host'], _config['database']['port']))
+
+        # (re)define self.db
+        self.db = dict()
+        self.db['client'] = _client
+        self.db['db'] = _db
+        self.db['coll_obs'] = _coll_obs
+        self.db['coll_aux'] = _coll_aux
+        self.db['program_pi'] = _program_pi
+
+    def disconnect_from_db(self):
+        """
+            Disconnect from Robo-AO's MongoDB database.
+        :return:
+        """
+        self.logger.debug('Disconnecting from the database.')
+        if self.db is not None:
+            try:
+                self.db['client'].close()
+                self.logger.debug('Successfully disconnected from the database.')
+            except Exception as e:
+                self.logger.error('Failed to disconnect from the database.')
+                self.logger.error(e)
+            finally:
+                # reset
+                self.db = None
+        else:
+            self.logger.debug('No connection found.')
+
+    def check_db_connection(self):
+        """
+            Check if DB connection is alive/established.
+        :return: True if connection is OK
+        """
+        self.logger.debug('Checking database connection.')
+        if self.db is None:
+            try:
+                self.connect_to_db()
+            except Exception as e:
+                print('Lost database connection.')
+                self.logger.error('Lost database connection.')
+                self.logger.error(e)
+                return False
+        else:
+            try:
+                # force connection on a request as the connect=True parameter of MongoClient seems
+                # to be useless here
+                self.db['client'].server_info()
+            except pymongo.errors.ServerSelectionTimeoutError as e:
+                print('Lost database connection.')
+                self.logger.error('Lost database connection.')
+                self.logger.error(e)
+                return False
+
+        return True
+
+    def cycle(self):
+        """
+            Main processing cycle
+        :return:
+        """
+        ''' check if DB connection is alive/established '''
+        connected = self.check_db_connection()
+
+        if connected:
+            pass
+
+        self.sleep()
 
 
 def task_runner(argdict):
@@ -311,15 +622,37 @@ def job_bogus(a):
     return a
 
 
+# TODO:
+class Observation(object):
+    pass
+
+
+class Pipeline(object):
+    pass
+
+
 if __name__ == '__main__':
 
-    arch = RoboaoArchiver('config.json')
+    ''' Create command line argument parser '''
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     description='Manage data archive for Robo-AO')
 
-    # fake tasks:
-    tasks = [json.dumps({'task': 'test', 'a': aa}) for aa in range(20)]
-    tasks += [json.dumps({'task': 'bogus', 'a': aa}) for aa in range(20, 50)]
+    parser.add_argument('config_file', metavar='config_file',
+                        action='store', help='path to config file.', type=str)
 
-    for task in tasks:
-        arch.q.put(task)
+    args = parser.parse_args()
 
-    time.sleep(30)
+    # go-go-go
+    arch = RoboaoArchiver(args.config_file)
+
+    arch.cycle()
+
+    if False:
+        # fake tasks:
+        tasks = [json.dumps({'task': 'test', 'a': aa}) for aa in range(20)]
+        tasks += [json.dumps({'task': 'bogus', 'a': aa}) for aa in range(20, 50)]
+
+        for task in tasks:
+            arch.q.put(task)
+
+    # time.sleep(10)
