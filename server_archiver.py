ae6e329499cbc14b70c1fecef09a13852ec7a035
Dmitry Duev <dmitry.duev@gmail.com>
2017-11-04 00:41:01 -0700

    misc fixes

--- server_archiver.py
+++ server_archiver.py
@@ -1492,7 +1492,7 @@ def manage_users():
             if usr['programs'] == 'all':
                 _users[usr['_id']] = {'programs': ['all']}
             else:
-                _users[usr['_id']] = {'programs': [p.encode('ascii', 'ignore')[2:-1]
+                _users[usr['_id']] = {'programs': [p.encode('ascii', 'ignore')
                                                    for p in usr['programs']]}
 
         return flask.render_template('template-users.html',
